7a7d6dffb55018c766e68534d97e4fe55d56fc14
topex148 <47463902+topex148@users.noreply.github.com>
2019-08-21 13:55:14 -0400

    Second Commit

--- config/app.php
+++ config/app.php
@@ -39,7 +39,7 @@ return [
     |
     */
 
-    'debug' => env('APP_DEBUG', false),
+    'debug' => env('APP_DEBUG', true),
 
     /*
     |--------------------------------------------------------------------------
@@ -119,7 +119,7 @@ return [
     |
     */
 
-    'key' => env('APP_KEY'),
+    'key' => env('APP_KEY', 'base64:iPlkbNJ07fm1QpnmTFogVUDC5Q5Spw/CHChS4gcwk60='),
 
     'cipher' => 'AES-256-CBC',
 
